acb6191def7f8fbcc59c52e8c1334e6f50ac3fe0
Daniel <dpericich@gmail.com>
2020-01-17 23:26:36 -0600

    Committing months folder with all raw data for each month in csv format
    Also committing data cleaning file. Removed brute method for csv loading. Added in time manipulation and cleanup as well as starting to add in some other variables/generalized tags for reporting later

--- combineMonthReports.R
+++ combineMonthReports.R
@@ -1,52 +1,47 @@
-## Creating Year long csv file to analyze
+## Creating CLEAN Year long csv file to analyze
 
 ## Using stats probided by Metropolitan Police Department - City of St. Louis, Missouri
 ## Accessed 1/16/20 at ~17:16
 
-## First Load in all csv files
-## Clear out any noise/extra data from the beginning of the files
+## Load in tidyverse library
+library(tidyverse)
+library(lubridate)
 
-## January 2019 
-jan19 <- read.csv("January2019.csv", header = FALSE, skip = 3)
+## After storing all files in a single folder in the project, load all their names into a variable
+# setwd("months/")
+filenames <- list.files(all.files = TRUE)
+filenames <- filenames[-(1:2)]
 
-## February 2019
-feb19 <- read.csv("February2019.csv", header = FALSE, skip = 208)
+## Next combine all csv files
+## make sure to set header as false and skip the first row to get rid of the header
+All <- lapply(filenames, function(i){
+read.csv(i, header = FALSE, skip = 1)
+})
 
-## March 2019
-mar19 <- read.csv("March2019.csv", header = FALSE, skip = 268)
+## Transform "All" variable into a single dataframe
+df <- do.call(rbind.data.frame, All)
 
-## April 2019 
-apr19 <- read.csv("April2019.csv", header = FALSE, skip = 307)
+## Add in column headers to dataframe
+names(df) <- c("Complaint", "Coded_Month", "Date_Occurance", "Flag_Crime", "Flag_Unfounded",
+               "Flag_Administrative", "Count", "Flag_Cleanup", "Crime", "District", "Description",
+               "ILEADsAddress", "ILEADSStreet", "Neighborhood", "Location_Name", 
+               "Location_Comment", "CADAddress", "CADStreet", "XCoord", "YCoord")
 
-## May 2019
-may19 <- read.csv("May2019.csv", header = FALSE, skip = 313)
+## Convert data frame to a tibble
+df <- as.tibble(df)
 
-## June 2019
-jun19 <- read.csv("June2019.csv", header = FALSE, skip = 487)
+## Use Lubridate to cleanup "Date_Occurance" column
+df <- mutate(df, Date_Occurance = mdy_hm(Date_Occurance))
 
-## July 2019
-jul19 <- read.csv("July2019.csv", header = FALSE, skip = 343)
+## Arrange Occurances by Date and then remove any that do not happen within 2019
+df <- arrange(df, Date_Occurance)
 
-## August 2019
-aug19 <- read.csv("August2019.csv", header = FALSE, skip = 428)
+## Remove any dates that are not in the year 2019
+rows_remove <- nrow(filter(df, year(Date_Occurance) != 2019))
+df <- df[-(1:rows_remove),]
 
-## September 2019 
-sep19 <- read.csv("September2019.csv", header = FALSE, skip = 465)
+## Add Column that tells day of the week for crime
+df <- mutate(df, Day = wday(day(Date_Occurance), label = TRUE ))
 
-## October 2019
-oct19 <- read.csv("October2019.csv", header = FALSE, skip = 446)
-
-## November 2019
-nov19 <- read.csv("November2019.csv", header = FALSE, skip = 394)
-
-## December 2019
-dec19 <- read.csv("December2019.csv", header = FALSE, skip = 461)
-
-## Read in the January file to get a header object
-header <- read.csv("January2019.csv", header = FALSE, nrows = 1)
-
-## Store all months as an object
-months <- list(jan19, feb19, mar19, apr19, may19, jun19, jul19, aug19, sep19, oct19, nov19, dec19)
-
-## Combine months into a single data frame
-crime2019 <- merge(months, check.names = FALSE)
+## Generalize Crime in new column based off the first two digits of six digit code
+## Reference codebook in github account for codes
